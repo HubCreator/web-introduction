ce710b595c3a68252eeb15bae71cbcbd77df77fc
HubCreator <chocomilk8604@gmail.com>
2023-02-16 19:36:11 +0900

    feat(main.js): js 기능 구현

---
+++ js/main.js
@@ -0,0 +1,19 @@
+window.onload = () => {
+    let submitButton = document.getElementById("submitButton");
+    submitButton.addEventListener("click", clickEvent);
+};
+
+function clickEvent() {
+    const name = document.getElementById("inputName");
+    const checkbox = document.getElementsByClassName("checkbox");
+    const children = checkbox[0].children;
+    let count = 0;
+
+    for (i = 0; i < children.length; i++) {
+        if (children[i].checked) {
+            count++;
+        }
+    }
+
+    alert(name.value + ", 저와 " + count + "개의 취향이 같으시네요!");
+}
